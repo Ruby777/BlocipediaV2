a0a1225876cc40bb39536d2e35071278508af485
Lucy <lucynn11@gmail.com>
2018-11-30 18:51:17 +0400

    collaborator errors

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,11 +1,21 @@
 class CollaboratorsController < ApplicationController
-    def create
+    def index
+        @collaborators = Collaborator.all
+        @wiki = Wiki.find(params[:wiki_id])
+    end
+
+    def new
+        @wiki = Wiki.find(params[:wiki_id])
         @collaborator = Collaborator.new
-        @wiki = Wiki.find_by(id: params[:collaborator][:wiki_id])
+    end
+    def create
+        @wiki = Wiki.find(params[:collaborator][:wiki_id])
+        @users = User.find(params[:collaborator][:user_id])
+        @collaborator = @wiki.collaborators.new(user_id: params[:collaborator][:user_id])
        
         if @collaborator.save
             flash[:notice] = "The collaborator has been added."
-            redirect_to :index
+            redirect_to wikis_path
         else
             flash[:alert] = "There was an error while adding the collaborator. Please try again."
             render :show
@@ -13,7 +23,6 @@ class CollaboratorsController < ApplicationController
     end
 
     def destroy
-        @wiki = Wiki.find(params[:wiki_id])
         @collaborator = Collaborator.find(params[:id])
 
         if @collaborator.destroy
@@ -22,6 +31,6 @@ class CollaboratorsController < ApplicationController
             flash.now[:alert] = "There was an error while removing the Collaborator."
         end
 
-        redirect_to @wiki
+        redirect_to wikis_path
     end
 end
